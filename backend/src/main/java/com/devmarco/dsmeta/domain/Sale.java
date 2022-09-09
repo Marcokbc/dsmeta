9032b3e7251ad3b8614d44ab92b277bae1fadaf8
Marco <MAMeira55@gmail.com>
2022-09-09 18:04:20 -0300

    Database

---
+++ backend/src/main/java/com/devmarco/dsmeta/domain/Sale.java
@@ -0,0 +1,30 @@
+package com.devmarco.dsmeta.domain;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_sales")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sale {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+}
